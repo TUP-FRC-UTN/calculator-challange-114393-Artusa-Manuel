753179a4342cf7dbbd758caa538252964a7215c2
114393-Artusa-Manuel <114393@tecnicatura.frc.utn.edu.ar>
2024-08-23 17:04:02 -0300

    First

---
+++ src/app/calc-actions/calc-actions.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-actions',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc-actions.component.html',
+  styleUrl: './calc-actions.component.css'
+})
+export class CalcActionsComponent {
+
+}
---
+++ src/app/calc-view/calc-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './calc-view.component.html',
+  styleUrl: './calc-view.component.css'
+})
+export class CalcViewComponent {
+
+}
