8c1950ab0c36bae05e29e2ce26e7499708cd2f70
114393-Artusa-Manuel <manu.artusa@gmail.com>
2024-08-24 11:44:25 -0300

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CalcViewComponent } from './calc-view/calc-view.component';
+import { FormsModule } from '@angular/forms';
+import { CalcActionsComponent } from './calc-actions/calc-actions.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CalcViewComponent],
+  imports: [RouterOutlet,CalcViewComponent,FormsModule,CalcActionsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'calculator';
+
+  valor1: number = 0;
+  valor2: number = 0;
+
+  resultado!: number;
+
+  actualizarResult(resultado:number){
+    this.resultado = resultado
+
+  }
 }
--- src/app/calc-actions/calc-actions.component.ts
+++ src/app/calc-actions/calc-actions.component.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-calc-actions',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './calc-actions.component.html',
   styleUrl: './calc-actions.component.css'
 })
 export class CalcActionsComponent {
+  @Output() resultadoEvent = new EventEmitter<number>();
+
+  valor1 : number =0
+  
+  valor2 : number =0
+
+  resultado: number = 0
+  operar(operacion: string, valor1: number, valor2: number){
+    let resultado: number = 0
+    switch(operacion){
+
+      case 'suma': resultado = valor1 + valor2
+      break;
+      
+      case 'resta': resultado = valor1 - valor2
+      break;
+      
+      case 'multiplicacion': resultado = valor1 * valor2
+      break;
+      
+      case 'division': resultado = valor1 / valor2
+      break;
+      
+      case 'potencia': resultado = Math.pow(valor1,valor2)
+      break;
+      
+      case 'raiz':resultado = Math.pow(valor1, 1 / valor2)
+      break;
+    }
+    this.resultado = resultado
+    this.resultadoEvent.emit(resultado);
+  }
+  clear(){
+    this.valor1 = 0
+    this.valor2 = 0
+    this.resultado = 0
+  }
 
 }
